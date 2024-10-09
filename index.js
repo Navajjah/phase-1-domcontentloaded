25cac9429b0fbe0cef7e780c2a3cc77cb1c6341c
Debrah Muinde <debbynav645@gmail.com>
2024-10-09 10:30:51 +0300

    Updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    const textReplace = document.getElementById('text');
+    textReplace.textContent = 'This is really cool!';
+    console.log(textReplace)
+})
